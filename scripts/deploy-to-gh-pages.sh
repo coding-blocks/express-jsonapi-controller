b3b5bc27d72b3c80736111dd02d67f1d5357a44b
Sagnik Mandal <sonalisagnik123@gmail.com>
2019-05-18 16:12:43 +0530

    Setup Hexo
    Fix Typo
    
    Update deploy-to-gh-pages.sh
    
    Update .travis.yml
    
    Update .travis.yml
    
    Fix Typo

---
+++ scripts/deploy-to-gh-pages.sh
@@ -0,0 +1,18 @@
+set -o errexit
+
+rm -rf public
+mkdir public
+
+# config
+git config --global user.email "nobody@nobody.org"
+git config --global user.name "Travis CI"
+
+# build (CHANGE THIS)
+yarn docs
+
+# deploy
+cd public
+git init
+git add .
+git commit -m "Deploy to Github Pages"
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
